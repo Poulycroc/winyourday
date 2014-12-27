9b5ea10880be0708bf111e25750032534856a6b2
Banjopalmo <palmerobenjamin@gmail.com>
2014-12-27 23:48:59 +0100

    Défi du jour
    Page défi du jour + accepter/ refuser -> photos -> page photos défi du
    jour + bouton retour. Le tout animé dans la joie et la bonne humeur.

--- assets/js/main - Benji.js
+++ assets/js/main - Benji.js
@@ -1,9 +1,32 @@
-var main = document.getElementsByClassName("defi_du_jour_accueil"),
-	photos = document.getElementsByClassName("photos_defi_du_jour"),
-	//btnCloser = document.getElementsByClassName("closer"),
-	btnOpener = document.getElementsByClassName("accepter_defi_jour");
+var accepter = document.getElementsByClassName("accepter_defi_jour"),
+	refuser = document.getElementsByClassName("refuser_defi_jour"),
+	photos = document.getElementsByClassName("bouton_photos_defi"),
+	photos_du_jour = document.getElementsByClassName("photos_defi_du_jour"),
+	defi_du_jour = document.getElementsByClassName("defi_du_jour_accueil");
+	bouton_retour = document.getElementsByClassName("bouton_retour")
 
-btnOpener[0].addEventListener("click", function(){
-	main[0].classList.add("photos--open");
-	photos[0].classList.add("photos--open");
+
+accepter[0].addEventListener("click", function(){
+	accepter[0].classList.add("activated");
+	refuser[0].classList.add("activated");
+	photos[0].classList.add("activated");
 });	
+
+
+refuser[0].addEventListener("click", function(){
+	accepter[0].classList.add("activated");
+	refuser[0].classList.add("activated");
+	photos[0].classList.add("activated");
+});	
+
+photos[0].addEventListener("click", function(){
+	photos_du_jour[0].classList.add("open");
+	defi_du_jour[0].classList.add("open");
+	bouton_retour[0].classList.add("open");
+});
+
+bouton_retour[0].addEventListener("click", function(){
+	photos_du_jour[0].classList.remove("open")
+	defi_du_jour[0].classList.remove("open");
+	bouton_retour[0].classList.remove("open");
+})
