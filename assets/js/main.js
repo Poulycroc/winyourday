6c3fc5338f5c514962df4ae1f4b27643c761b5e9
robindevouge <robin.devouge@gmail.com>
2015-01-05 12:38:02 +0100

    adding animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
 
-	
+// LOGIN
+
+	$(".noAccount").click(function () {
+		$('.labelEmail').addClass('show');
+		$('.mail').addClass('show');
+		$('.btn__login--inscription').addClass('show');
+
+		$('.btn__login--connexion').addClass('hide');
+		$('.noAccount').addClass('hide');
+	});
+
+
+
+// ANIMS
+
+	// PAGE SWITCH
+
+	$(".btn__next").on('click', function () {
+		event.preventDefault();
+	    $(this).parents('.page__profile').removeClass('displayed');
+	    $(this).parents('.page__profile').siblings('.page').addClass('displayed');
+	});
+
+	// PAGE SLIDE
+
+	$(".btn__loginOpen").on('click', function () {
+		event.preventDefault();
+	    $(this).parents('.page__profile').addClass('hidden');
+	    $('.login').addClass('active');
+	});
+
+	$(".btn__login").on('click', function () {
+		event.preventDefault();
+	    $('.hidden').removeClass('hidden');
+	    $('.login').removeClass('active');
+	});
+
 
 });
