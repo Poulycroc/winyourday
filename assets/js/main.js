943d65cbabb48e9573f8696be7400422b786eb7d
robindevouge <robin.devouge@gmail.com>
2015-01-06 10:38:11 +0100

    making the whole thing work with framework

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
 
+	// LOGIN
+
+	$(".noAccount, .alreadyAccount").click(function () {
+		$('.btn__login--connexion, .noAccount, .labelEmail, .mail, .btn__login--inscription, .alreadyAccount').toggleClass('hide');
+	});
+
 	
+
+// ANIMS
+
+	// PAGE SWITCH
+
+	$(".btn__next").on('click', function () {
+		event.preventDefault();
+	    $(this).parents('.page__profile').removeClass('displayed');
+	    $(this).parents('.page__profile').siblings('.page').addClass('displayed');
+	});
+
+	// PAGE SLIDE
+
+	$(".btn__loginOpen").on('click', function () {
+		event.preventDefault();
+	    $(this).parents('.page__profile').addClass('hidden');
+	    $('.login').addClass('active');
+	});
+
+	$(".btn__login").on('click', function () {
+		event.preventDefault();
+	    $('.hidden').removeClass('hidden');
+	    $('.login').removeClass('active');
+	});
 
 });
