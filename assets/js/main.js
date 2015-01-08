10c9c11c952b0743f8482bc55a499392b7e8397d
robindevouge <robin.devouge@gmail.com>
2015-01-08 09:50:41 +0100

    some not-working-code
    form plugin to remove ?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,120 @@
 $(document).ready(function() {
 
-	// LOGIN
+// LOCAL STORAGE
 
-	$(".accountStatus").click(function () {
-		$('.mail').toggleClass('roll');
-		$('.accountStatus, .btn__login--connexion, .btn__login--inscription').toggleClass('slide');
+	var flagLoggedIn = 0;
+		flagUsername = 0;
+		flagPassword = 0;
+		flagMail = 0;
+		username = document.getElementById("username");
+		password = document.getElementById("password");
+		mail = document.getElementById("mail");
+
+	// INSCRIPTION
+
+	$(".btn__login--inscription").click( function (){
+
+		// testing the username value
+		
+		if (username.value.length >= 4 & username.value.length <= 20){
+			console.log("username ok")
+			localStorage.setItem("user", username.value);
+			flagUsername = 1;
+		}else{
+			$("#username").attr("placeholder", "I must be 4 to 20 charact.");
+		}
+
+		// testing the password value
+		if (password.value.length >= 8 & password.value.length <= 25){
+			console.log("password ok")
+			localStorage.setItem("password", password.value);
+			flagPassword = 1;
+		}
+
+		if (mail.value !== null || mail.value !== ""){
+			flagMail = 1;
+		}
+
+		// checking if all fields are filled
+		if (flagUsername == 1 & flagPassword == 1 & flagMail == 1){
+			flagLoggedIn = 1
+			console.log("login complete")
+		}
+
 	});
 
-	
+		// // VALIDATE PLUGIN
+
+		// $(".login__form").validate({
+		// 	rules: {
+		// 		username: {
+		// 			required: true,
+		// 			minlength: 4,
+		// 			maxlength: 20
+		// 	    }
+		// 	    password: {
+		// 			required: true,
+		// 			minlength: 4,
+		// 			maxlength: 20
+		// 	    }
+		// 	    mail: {
+		// 			required: true,
+		// 			email: true
+		// 	    }
+		// 	}
+
+		// 	messages: {
+		// 		username: {
+		// 			required: "Ce champ est requis",
+		// 			minlength: "Doit être 4 à 20 caract.",
+		// 			maxlength: "Doit être 4 à 20 caract."
+		// 		}
+		// 		password: {
+		// 			required: "Ce champ est requis",
+		// 			minlength: "Doit être 4 à 20 caract.",
+		// 			maxlength: "Doit être 4 à 20 caract."
+		// 		}
+		// 		mail: {
+		// 			required: "Ce champ est requis"
+		// 		}
+		// 	}
+
+		// });
+
+	// CONNEXION
+
+	// $(".btn__login--connexion").click( function(){
+
+	// 	localStorage.setItem("password", password.value);
+
+	// });
 
 // ANIMS
 
 	// PAGE SLIDE
 
-	$(".btn__loginOpen").on('click', function () {
+
+
+	$(".btn__loginOpen").click( function () {
 		event.preventDefault();
 	    $(this).parents('.page__profile').addClass('hidden');
 	    $('.login').addClass('active');
 	});
 
-	$(".btn__login").on('click', function () {
+	$(".btn__login").click( function () {
 		event.preventDefault();
-	    $('.hidden').removeClass('hidden');
-	    $('.login').removeClass('active');
+		if (flagLoggedIn == 1){
+		    $('.hidden').removeClass('hidden');
+		    $('.login').removeClass('active');
+		}
+	});
+
+	// LOGIN INSIDE PAGE
+
+	$(".accountStatus").click( function () {
+		event.preventDefault();
+		$('.mail').toggleClass('roll');
+		$('.accountStatus, .btn__login--connexion, .btn__login--inscription').toggleClass('slide');
 	});
 
 });
